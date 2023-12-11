622bda85e14354f050d60757ad4276cdbc0d54ee
Alex Moreno <alex.moreno32390@gmail.com>
2023-12-11 17:58:08 +0100

    Affichage de tous les cd

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-
 <?php
-echo "hello world !";
-?>
 
+$json_file = "articles.json";
+$articles = file_get_contents($json_file);
+$liste_articles = json_decode($articles, true);
+
+// Vérifier si la conversion a réussi
+if ($articles === null) {
+    die('Erreur de décodage JSON');
+}
+
+// Parcourir les CDs
+foreach ($liste_articles['cds'] as $cd) {
+    echo "ID: {$cd['id']}<br>";
+    echo "Genre: {$cd['genre']}<br>";
+    echo "Title: {$cd['title']}<br>";
+    echo "Artist: {$cd['artist']}<br>";
+    echo "Price: {$cd['price']}<br>";
+    echo "Image: {$cd['image']}<br>";
+    echo "<hr>";
+}
+?>
