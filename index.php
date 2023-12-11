ae8913d0f5409a0b16edec26d89b856feea489a6
Alex Moreno <alex.moreno32390@gmail.com>
2023-12-11 18:25:39 +0100

    Génération du code des cartes CD

--- index.php
+++ index.php
@@ -5,17 +5,24 @@ $articles = file_get_contents($json_file);
 $liste_articles = json_decode($articles, true);
 
 // Vérifier si la conversion a réussi
-if ($articles === null) {
+if ($liste_articles === null) {
     die('Erreur de décodage JSON');
 }
 
-// Parcourir les CDs
+// Parcourir les CDs et générer les liens avec les cartes
 foreach ($liste_articles['cds'] as $cd) {
-    print("p");
-    print("<a href=''>{$cd['genre']}<br></a>");
-    print("<a href=''>{$cd['title']}<br></a>");
-    print("<a href=''>{$cd['artist']}<br></a>");
-    print("<a href=''>{$cd['genre']}<br></a>");
-    print("<a href=''>{$cd['price']}<br></a>");
+    echo '<a href="page_produit.php?id=' . $cd['id'] . '" class="cd-link">';
+    echo '<div class="cd-card">';
+    echo '<img src="Images/' . $cd['image'] . '" alt="' . $cd['title'] . '">';
+    echo '<h3>' . $cd['title'] . '</h3>';
+    echo '<p>Genre: ' . $cd['genre'] . '</p>';
+    echo '<p>Artist: ' . $cd['artist'] . '</p>';
+    echo '<p>Price: $' . $cd['price'] . '</p>';
+    echo '</div>';
+    echo '</a>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
 }
 ?>
