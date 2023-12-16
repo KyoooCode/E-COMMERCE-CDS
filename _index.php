cd7b362635158947a7bfd45e1dfd8166c0588715
Grigori Gratchev <123749002+jrijori@users.noreply.github.com>
2023-12-16 17:11:56 +0100

    ajout d'un _ pour fichiers pour la version bdd du projet, ajout connexion avec verification

--- _index.php
+++ _index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des Produits</title>
+    <title>Tous les Produits</title>
 </head>
 <body>
+
+<h1>Tous les Produits</h1>
 
 <?php
 $servername = "lakartxela.iutbayonne.univ-pau.fr:3306";
@@ -19,23 +21,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM PRODUIT";
+$sql = "SELECT genre, libelle, artiste, prix, chemin FROM PRODUIT";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table border='1'><tr><th>ID</th><th>Genre</th><th>Libellé</th><th>Artiste</th><th>Prix</th><th>Image</th></tr>";
+    echo "<table border='1'><tr><th>Genre</th><th>Libellé</th><th>Artiste</th><th>Prix</th><th>Image</th></tr>";
 
     while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["genre"] . "</td><td>" . $row["libelle"] . "</td><td>" . $row["artiste"] . "</td><td>" . $row["prix"] . "</td><td><img src='Images/" . $row["chemin"] . "' alt='Image du produit' width='150' ></td></tr>";
+        echo "<tr><td>" . $row["genre"] . "</td><td>" . $row["libelle"] . "</td><td>" . $row["artiste"] . "</td><td>" . $row["prix"] . "</td><td><img src='Images/" . $row["chemin"] . "' alt='Image du produit' width='150' ></td></tr>";
     }
 
     echo "</table>";
 } else {
-    echo "0 results";
+    echo "Aucun résultat trouvé.";
 }
 
 $conn->close();
 ?>
+
+<p><a href="_connexion.html">Connexion</a></p>
 
 </body>
 </html>
---
+++ _verification.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les identifiants du formulaire
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Connexion à la base de données
+    $servername = "lakartxela.iutbayonne.univ-pau.fr:3306";
+    $dbUsername = "amoreno011_bd";
+    $dbPassword = "amoreno011_bd";
+    $dbName = "amoreno011_bd";
+
+    $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prévenir les attaques par injection SQL
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // Requête pour vérifier les identifiants dans la base de données
+    $sql = "SELECT * FROM UTILISATEUR WHERE login = '$username' AND mdp = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Si les identifiants correspondent, rediriger vers la page de gestion
+        header("Location: _gestion.html");
+        exit();
+    } else {
+        // Si les identifiants ne correspondent pas, rediriger vers la page d'erreur
+        header("Location: _IdIncorrects.html");
+        exit();
+    }
+
+    // Fermer la connexion à la base de données
+    $conn->close();
+}
+?>
